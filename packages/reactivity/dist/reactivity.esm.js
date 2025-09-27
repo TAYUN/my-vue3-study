f22e83d41825223bdc606dbc117a6d1fd57671e3
TAYUN <80144903+TAYUN@users.noreply.github.com>
2025-09-27 11:39:02 +0800

    feat(reactivity): 实现ref的依赖收集和触发更新功能
    添加activeSub全局变量用于跟踪当前effect
    为RefImpl类添加依赖收集和触发更新逻辑
    新增isRef方法用于判断是否为ref对象

--- packages/reactivity/dist/reactivity.esm.js
+++ packages/reactivity/dist/reactivity.esm.js
@@ -1,25 +1,43 @@
 // packages/reactivity/src/effect.ts
+var activeSub;
 function effect(fn) {
+  activeSub = fn;
   fn();
+  activeSub = void 0;
 }
 
 // packages/reactivity/src/ref.ts
 var RefImpl = class {
   _value;
+  // 保存实际值
+  // ref 标记，证明这是一个 ref 对象
+  ["__v_isRef" /* IS_REF */] = true;
+  subs;
   constructor(value) {
     this._value = value;
   }
+  // 收集依赖
   get value() {
+    if (activeSub) {
+      this.subs = activeSub;
+    }
     return this._value;
   }
+  // 触发更新
   set value(newValue) {
     this._value = newValue;
+    this.subs?.();
   }
 };
 function ref(value) {
   return new RefImpl(value);
 }
+function isRef(value) {
+  return !!(value && value["__v_isRef" /* IS_REF */]);
+}
 export {
+  activeSub,
   effect,
+  isRef,
   ref
 };
