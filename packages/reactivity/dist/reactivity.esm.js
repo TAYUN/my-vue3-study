c16c1508ebcc2aaa48e6259e5e7301fa10878d44
TAYUN <80144903+TAYUN@users.noreply.github.com>
2025-09-29 14:46:59 +0800

    refactor(reactivity): 重构RefImpl以支持多个订阅者
    将单个订阅者改为链表结构存储，支持多个effect订阅同一个ref。在get时添加新订阅者到链表尾部，set时遍历链表执行所有订阅effect。

--- packages/reactivity/dist/reactivity.esm.js
+++ packages/reactivity/dist/reactivity.esm.js
@@ -12,21 +12,42 @@ var RefImpl = class {
   // 保存实际值
   // ref 标记，证明这是一个 ref 对象
   ["__v_isRef" /* IS_REF */] = true;
+  // 订阅者effect链表头节点，指向第一个订阅者
   subs;
+  // 订阅者effect链表尾节点，指向最后一个订阅者
+  subsTail;
   constructor(value) {
     this._value = value;
   }
   // 收集依赖
   get value() {
     if (activeSub) {
-      this.subs = activeSub;
+      const newLink = {
+        sub: activeSub,
+        nextSub: void 0,
+        prevSub: void 0
+      };
+      if (this.subsTail) {
+        this.subsTail.nextSub = newLink;
+        newLink.prevSub = this.subsTail;
+        this.subsTail = newLink;
+      } else {
+        this.subs = newLink;
+        this.subsTail = newLink;
+      }
     }
     return this._value;
   }
   // 触发更新
   set value(newValue) {
     this._value = newValue;
-    this.subs?.();
+    let link = this.subs;
+    const queueEffect = [];
+    while (link) {
+      queueEffect.push(link.sub);
+      link = link.nextSub;
+    }
+    queueEffect.forEach((effect2) => effect2());
   }
 };
 function ref(value) {
