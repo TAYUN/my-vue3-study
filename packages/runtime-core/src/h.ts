b2236fc73d9b800220848f80e533c1022808130b
TAYUN <80144903+TAYUN@users.noreply.github.com>
2025-10-23 17:16:18 +0800

    feat(runtime-core): 添加虚拟节点和h函数支持
    实现虚拟节点创建函数createVNode和h函数，用于创建和标准化虚拟节点参数
    添加示例文件展示h函数使用方法

---
+++ packages/runtime-core/src/h.ts
@@ -0,0 +1,56 @@
+import { isArray, isObject } from '@vue/shared'
+import { createVNode, isVNode } from './vnode'
+
+/**
+ * h 函数的使用方法：
+ * 1. h('div', 'hello world') 第二个参数为 子节点
+ * 2. h('div', [h('span', 'hello'), h('span', ' world')]) 第二个参数为 子节点
+ * 3. h('div', h('span', 'hello')) 第二个参数为 子节点
+ * 4. h('div', { class: 'container' }) 第二个参数是 props
+ * ------
+ * 5. h('div', { class: 'container' }, 'hello world')
+ * 6. h('div', { class: 'container' }, h('span', 'hello world'))
+ * 7. h('div', { class: 'container' }, h('span', 'hello'), h('span', 'world'))
+ * 8. h('div', { class: 'container' },[h('span', 'hello'), h('span', 'world')]) 和 7 一个意思
+ */
+
+export function h(type, propsOrChildren?, children?) {
+  /**
+   * h 函数，主要的作用是对 createVNode 做一个参数标准化（归一化）
+   */
+
+  let l = arguments.length
+
+  if (l === 2) {
+    if (isArray(propsOrChildren)) {
+      // h('div', [h('span', 'hello'), h('span', ' world')])
+      return createVNode(type, null, propsOrChildren)
+    }
+
+    if (isObject(propsOrChildren)) {
+      if (isVNode(propsOrChildren)) {
+        // h('div', h('span', 'hello'))
+        return createVNode(type, null, [propsOrChildren])
+      }
+      // h('div', { class: 'container' })
+      return createVNode(type, propsOrChildren, children)
+    }
+
+    // h('div', 'hello world')
+    return createVNode(type, null, propsOrChildren)
+  } else {
+    if (l > 3) {
+      /**
+       * h('div', { class: 'container' }, h('span', 'hello'), h('span', 'world'))
+       * 转换成
+       * h('div', { class: 'container' }, [h('span', 'hello'), h('span', 'world')])
+       */
+      children = [...arguments].slice(2)
+    } else if (isVNode(children)) {
+      // h('div', { class: 'container' }, h('span', 'hello world'))
+      children = [children]
+    }
+    // 要是只传了 type
+    return createVNode(type, propsOrChildren, children)
+  }
+}
